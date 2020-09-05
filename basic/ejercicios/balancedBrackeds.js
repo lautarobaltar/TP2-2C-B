9a1a75ee4eeb0ff3960713af4128543553a368e4
lautarobaltar <lautaro.baltar3@gmail.com>
2020-09-05 04:34:56 -0300

    Desafio #1 - Completo

--- basic/ejercicios/balancedBrackeds.js
+++ basic/ejercicios/balancedBrackeds.js
@@ -1,7 +1,58 @@
+// Creating Stack class for generic use
+class Stack {
+    constructor(){
+        this.data = [];
+        this.top = 0;
+    }
+
+    push(element) {
+        this.data[this.top] = element;
+        this.top += 1;
+    }
+
+    peek() {
+        return this.data[this.top -1 ];
+    }
+
+    isEmpty() {
+        return this.top === 0;
+    }
+
+    pop() {
+        if( this.isEmpty() === false ) {
+           this.top = this.top -1;
+           return this.data.pop();
+         }
+    }
+}
+
+
+
 // Complete the isBalanced function below.
 function isBalanced(s) {
-
-
+    var openers = ["(", "{", "["]
+    var closers = [")", "}", "]"]
+    var search = new Stack();
+    for(var c in s){
+        if (openers.includes(s[c])){
+            search.push(s[c])
+        } else {
+            if( search.isEmpty){
+                var current = search.peek()
+                if(openers.indexOf(current) == closers.indexOf(s[c])){
+                    search.pop()
+                } else {
+                    return "NO";
+                }
+            } else {
+                return "NO";
+            }
+        }
+    }
+    if(search.isEmpty()){
+        return "YES";
+    }
+    return "NO";
 }
 
 // TESTS
